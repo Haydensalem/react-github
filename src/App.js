20187dae1b29f5fee7fb4d43a4ad812ffde750bc
hayden <haydenrsalem@gmail.com>
2018-01-25 22:59:07 -0800

    added location and public repos

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ class App extends Component {
   }
 
   getData() {
-    let gitHubusername = this.refs.name.value;
-    axios.get('https://api.github.com/users/' + gitHubusername ).then((res) =>{
+    let gitHubusername = this.refs.name.value; 
+    axios.get('https://api.github.com/users/' + gitHubusername ).then((res) => {
       console.log(res.data);
       this.setState({ user: res.data})
     })
@@ -21,26 +21,19 @@ class App extends Component {
   render() {
     const  { user } = this.state;
     return (
-      <div className='col-md-6'>
+      <div className=' col-md-offset-3 col-md-6'>
         <div className='form-group'>
           <input className='form-control' type="text" placeholder=" Enter Github Username " ref="name"/>
         </div>
-        <button className='btn btn-primary' onClick={this.getData.bind(this)}>Call the bitch</button>
-        { user.login ? <UserDeatils user={user}/>  : <p>User not found</p> }
-        </div>
+        <button className='btn btn-primary col-md-offset-5 ' onClick={this.getData.bind(this)}>Call The User</button>
+        
+        <br />
+        <h1 className="col-md-offset-4" >{user.login}</h1>
+        {user.login ? <p className="col-md-offset-4"><span> Location = {user.location}</span> <br /> <span> Public Repos = {user.public_repos}</span></p> : ''}
+        <img className="col-md-offset-4" src={user.avatar_url} alt={user.login}/> 
+      </div>
     );
   }
 }
 
 export default App;
-
-const UserDeatils = ({user}) =>{
-  return (
-    <div>
-      <hr />
-      <h1>{user.login}</h1>
-      <img src={user.avatar_url} alt={user.login} />
-      <button className='btn btn-primary'>Get {user.login} repos</button>
-    </div>
-  )
-}
